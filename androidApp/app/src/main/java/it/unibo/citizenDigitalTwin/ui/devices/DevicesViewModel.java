ef6a43177adb98ce679e48ddecac4d913902c912
Francesco Grandinetti <francesco.grandinett@gmail.com>
2020-05-21 15:42:23 +0200

    init android project

---
+++ androidApp/app/src/main/java/it/unibo/citizenDigitalTwin/ui/devices/DevicesViewModel.java
@@ -0,0 +1,19 @@
+package it.unibo.citizenDigitalTwin.ui.devices;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DevicesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DevicesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is devices fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
