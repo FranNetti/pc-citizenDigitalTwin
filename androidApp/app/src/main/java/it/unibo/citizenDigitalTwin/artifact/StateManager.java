9308d0b787b4fbb1ffdb82a60cb1a458b12881ed
Francesco Grandinetti <francesco.grandinett@gmail.com>
2020-06-05 20:43:11 +0200

    manage notifications in StateManager

--- androidApp/app/src/main/java/it/unibo/citizenDigitalTwin/artifact/StateManager.java
+++ androidApp/app/src/main/java/it/unibo/citizenDigitalTwin/artifact/StateManager.java
@@ -3,15 +3,12 @@ package it.unibo.citizenDigitalTwin.artifact;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import cartago.INTERNAL_OPERATION;
 import cartago.OPERATION;
 import cartago.ObsProperty;
 import it.unibo.citizenDigitalTwin.data.State;
-import it.unibo.citizenDigitalTwin.data.category.LeafCategory;
 import it.unibo.citizenDigitalTwin.data.connection.LoginResult;
 import it.unibo.citizenDigitalTwin.db.dao.NotificationDAO;
 import it.unibo.citizenDigitalTwin.db.entity.notification.DataNotification;
@@ -37,17 +34,6 @@ public class StateManager extends JaCaArtifact {
     private DataDAO dbState;
     private NotificationDAO dbNotifications;
 
-    private final List<Notification> notifications = Arrays.asList(
-            new DataNotification("Pippo e Minnie", Arrays.asList(LeafCategory.NAME)),
-            new MessageNotification("Cicciolina", "Vienimi a prendere fustacchione"),
-            new DataNotification("Paperino", Arrays.asList(LeafCategory.BIRTHDATE)),
-            new MessageNotification("Charles Leclerc", "Corriamo insieme!!!"),
-            new DataNotification("Pluto", Arrays.asList(LeafCategory.ADDRESS)),
-            new MessageNotification("Stefano Righini", "Vieni a recuperare i prodotti della mia terra"),
-            new DataNotification("Topolino", Arrays.asList(LeafCategory.SURNAME)),
-            new MessageNotification("Dottor Filippone", "Hai il Covid-19 coglione")
-    );
-
     public void init() {
         final AppDatabase db = AppDatabase.getInstance(getApplicationContext());
         this.dbState = db.dataDao();
@@ -69,8 +55,6 @@ public class StateManager extends JaCaArtifact {
         dbNotifications.getAllDataNotifications().forEach(notifications -> {
             updatePropNotification(notifications, DataNotification.class);
         });
-
-        execInternalOp("pippo");
     }
 
     @OPERATION
@@ -100,7 +84,21 @@ public class StateManager extends JaCaArtifact {
 
     @OPERATION
     public void addNotifications(final List<Notification> notifications){
-
+        final List<MessageNotification> msgNotifications = new ArrayList<>();
+        final List<DataNotification> dataNotifications = new ArrayList<>();
+        notifications.forEach(x -> {
+            switch (x.getType()){
+                case DATA: dataNotifications.add((DataNotification)x); break;
+                case MESSAGE: msgNotifications.add((MessageNotification)x); break;
+                default: Log.e(TAG, "Unhandled notification in addNotifications: " + x.getType());
+            }
+        });
+        if(!msgNotifications.isEmpty()){
+            dbNotifications.insertMessageNotifications(msgNotifications);
+        }
+        if(!dataNotifications.isEmpty()){
+            dbNotifications.insertDataNotifications(dataNotifications);
+        }
     }
 
     @OPERATION
@@ -121,19 +119,6 @@ public class StateManager extends JaCaArtifact {
         if(!dataNotifications.isEmpty()){
             dbNotifications.updateDataNotifications(dataNotifications);
         }
-    }
-
-    @INTERNAL_OPERATION
-    void pippo(){
-        final List<DataNotification> dN = new ArrayList<>();
-        final List<MessageNotification> mN = new ArrayList<>();
-        notifications.forEach(x -> {
-            if(x instanceof MessageNotification){
-                dbNotifications.insertMessageNotification((MessageNotification)x);
-            } else {
-                dbNotifications.insertDataNotification((DataNotification)x);
-            }
-        });
     }
 
     private void updatePropNotification(final List<? extends Notification> notifications, final Class<?> notClass){
--- androidApp/app/src/main/java/it/unibo/citizenDigitalTwin/db/dao/NotificationDAO.java
+++ androidApp/app/src/main/java/it/unibo/citizenDigitalTwin/db/dao/NotificationDAO.java
@@ -4,14 +4,12 @@ import java.util.Collection;
 import java.util.List;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 import io.reactivex.Flowable;
 import it.unibo.citizenDigitalTwin.db.entity.notification.DataNotification;
 import it.unibo.citizenDigitalTwin.db.entity.notification.MessageNotification;
-import it.unibo.citizenDigitalTwin.db.entity.notification.Notification;
 
 @Dao
 public interface NotificationDAO {
@@ -23,10 +21,10 @@ public interface NotificationDAO {
     Flowable<List<DataNotification>> getAllDataNotifications();
 
     @Insert
-    void insertMessageNotification(MessageNotification notification);
+    void insertMessageNotifications(Collection<MessageNotification> notification);
 
     @Insert
-    void insertDataNotification(DataNotification notification);
+    void insertDataNotifications(Collection<DataNotification> notification);
 
     @Update
     void updateMessageNotifications(Collection<MessageNotification> notifications);
